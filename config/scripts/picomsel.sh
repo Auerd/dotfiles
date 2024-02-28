ba51ec48ae9d601d463a441244934cf43ad0a542
Auerd <by3kov8roslav@gmail.com>
2024-02-29 01:41:32 +0400

    picomsel.sh script work normally now. Setup script will be splitted in separate files. Calibre folder was added to .gitignore

--- config/scripts/picomsel.sh
+++ config/scripts/picomsel.sh
@@ -1,6 +1,11 @@
 wdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
 clearver=$(/bin/sh $wdir/openglver.sh | sed "s/\.//")
-if [ "$clearver" -ge "33" ]; 
-	then picom -b;
-	else picom -b --config $HOME/.config/noglxpicom.conf;
-fi;	
+noglxpicom=$HOME/.config/noglxpicom.conf
+[[ -a $XDG_CONFIG_HOME ]] && noglxpicom=$XDG_CONFIG_HOME/noglxpicom.conf;
+
+if [[ "$clearver" -ge "33" ]]
+then 
+  picom -b
+else 
+  picom -b --config $noglxpicom
+fi
