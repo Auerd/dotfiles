06a5e8dfe4a8a718ab3c993b31ac466a2a72c1e1
Auerd <by3kov8roslav@gmail.com>
2024-04-19 22:08:43 +0400

    nvim-tree opens when argument is directory

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,6 +1,25 @@
 require "nvchad.options"
 
--- add yours here!
+local function open_nvim_tree(data)
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- create a new, empty buffer
+  vim.cmd.enew()
+
+  -- wipe the directory buffer
+  vim.cmd.bw(data.buf)
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
