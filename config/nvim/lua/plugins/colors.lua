bc280118e20f4469e02fc85a30c9d02e6f65c89d
Auerd <by3kov8roslav@gmail.com>
2024-05-07 16:06:35 +0400

    neovim. colors.lua reformatted a bit

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -1,17 +1,11 @@
-local function is_termux()
-	if vim.env.TERMUX_VERSION ~= nil then
-		return true
-	end
-	return false
-end
 return{
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = true,
-		event = "VeryLazy",
-		opts = {
-			no_italic = is_termux(),
-		},
-	}
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    event = "VeryLazy",
+    opts = {
+      no_italic = vim.env.TERMUX_VERSION ~= nil,
+    },
+  }
 }
