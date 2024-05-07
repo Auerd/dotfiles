726d247fca45599d62de0995c0e2c4090bd4596f
Auerd <by3kov8roslav@gmail.com>
2024-05-07 22:43:56 +0400

    neovim. Treesitter additional vim regex highlighting was disabled

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -46,7 +46,10 @@ return {
     ---@diagnostic disable-next-line: missing-fields
     opts = {
       sync_install = false,
-      highlight = { enable = true },
+      highlight = { 
+        enable = true, 
+        additional_vim_regex_highlighting = false,
+      },
       indent = { enable = true },
       ensure_installed = { 
         "c", "cpp", 
