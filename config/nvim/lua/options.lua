79230f473f313121e9e371ef7946dd4ec55f1efb
Auerd <by3kov8roslav@gmail.com>
2024-05-09 10:18:19 +0400

    neovim. options.lua was reformatted a bit

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,6 +1,16 @@
 local cmd = vim.cmd
+local o = vim.o
+
 
 cmd.colorscheme("github_dark_high_contrast")
-cmd.set("number")
-cmd.set("softtabstop=0 expandtab smarttab shiftwidth=2")
-cmd.set("laststatus=3")
+
+o.number = true
+
+-- Tabs
+o.softtabstop = 0
+o.expandtab = true
+o.smarttab = true
+o.shiftwidth = 2
+
+-- Statusline
+o.laststatus = 3
