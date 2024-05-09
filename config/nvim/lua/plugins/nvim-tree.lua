88a20e9542337b2a66fdfcba40437cf47b6d5892
Auerd <by3kov8roslav@gmail.com>
2024-05-09 09:30:57 +0400

    neovim. Nvimtree plugin was added

---
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    name = "nvim-tree",
+    opts = {
+      view = {
+        side = "right",
+        centralize_selection = true,
+        width = "10%",
+      }
+    },
+  }
+}
