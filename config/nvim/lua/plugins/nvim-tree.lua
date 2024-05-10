5ada45fe14ea75449f5e87cfa32a131d6f4041e7
Auerd <by3kov8roslav@gmail.com>
2024-05-10 12:51:36 +0400

    nvim-tree. Hijack cursor option was deleted

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -2,13 +2,11 @@ return {
   {
     "nvim-tree/nvim-tree.lua",
     version = "*",
-    lazy = false,
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
     name = "nvim-tree",
     opts = {
-      hijack_cursor = true,
       view = {
         side = "right",
         width = "15%",
