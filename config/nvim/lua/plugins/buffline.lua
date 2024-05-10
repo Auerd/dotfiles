16f9af986cf051f604a9b8cf9829baa1dc332556
Auerd <by3kov8roslav@gmail.com>
2024-05-10 11:47:52 +0400

    neovim, bufferline. Italics will be disabled in termux

--- config/nvim/lua/plugins/buffline.lua
+++ config/nvim/lua/plugins/buffline.lua
@@ -2,19 +2,27 @@ return {
   {
     "akinsho/bufferline.nvim",
     version = "*",
-    dependencies = "nvim-tree/nvim-web-devicons",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
     config = true,
-    opts = {
-      options = {
-        offsets = {
-          {
-            filetype = "NvimTree",
-            text = "File Explorer",
-            text_align = "center",
-            separator = true,
+    opts = function()
+      local bufferline = require "bufferline"
+      local default = bufferline.style_preset.default
+      local no_italic = bufferline.style_preset.no_italic
+      return {
+        options = {
+          style_preset = vim.g.no_italic and no_italic or default,
+          offsets = {
+            {
+              filetype = "NvimTree",
+              text = "File Explorer",
+              text_align = "center",
+              separator = true,
+            },
           },
         },
-      },
-    },
+      }
+    end,
   },
 }
