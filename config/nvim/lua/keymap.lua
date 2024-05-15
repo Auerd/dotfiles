72f83dca77a323c70acb8d0487b4ec525473bc3a
Auerd <by3kov8roslav@gmail.com>
2024-05-15 14:28:34 +0400

    neovim. Keymaps were added and changed Added keymaps for lsp defenition, references. Keymaps for buffer remove and lsp rename were changed

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -1,3 +1,5 @@
+-- Warning! This file executed before nvim. That means you can't use plugins there
+-- If it's needed to use plugin in keymap, you can map key in the init() function of plugin
 vim.cmd.let "mapleader = ' '"
 
 local map = vim.keymap.set
@@ -11,9 +13,22 @@ map("n", "<leader>e", function()
   require("nvim-tree.api").tree.toggle()
 end, { desc = "Toggle nvim-tree" })
 
-map("n", "<leader>a", function()
+-- But you can use this workaround
+map("n", "<leader>lr", function()
   return ":IncRename " .. vim.fn.expand "<cword>"
 end, { desc = "Lsp rename", expr = true })
+
+map("n", "<leader>lf", function()
+  vim.lsp.buf.references()
+end, { desc = "Lsp references" })
+
+map("n", "<leader>ld", function()
+  vim.lsp.buf.definition()
+end, { desc = "Lsp definitions" })
+
+map("n", "<leader>nn", function()
+  return vim.o.number and "<cmd>setlocal nonumber<cr>" or "<cmd>setlocal number<cr>"
+end, { desc = "Toggle lines number", expr = true })
 
 map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Go to next buffer" })
 map("n", "<S-Tab>", "<cmd>bprev<cr>", { desc = "Go to previous buffer" })
--- config/nvim/lua/plugins/bufdelete.lua
+++ config/nvim/lua/plugins/bufdelete.lua
@@ -2,7 +2,7 @@ return {
   {
     "famiu/bufdelete.nvim",
     init = function()
-      vim.keymap.set("n", "<leader>r", function()
+      vim.keymap.set("n", "<leader>d", function()
         require("bufdelete").bufdelete(0)
       end, { desc = "Delete current buffer and move to next" })
     end,
