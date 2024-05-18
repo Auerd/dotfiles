119416fc273a47e9adc88017a56ff3f321d0b1b8
Auerd <by3kov8roslav@gmail.com>
2024-05-18 10:08:27 +0400

    picomsel.sh was foramtted

--- config/scripts/picomsel.sh
+++ config/scripts/picomsel.sh
@@ -1,11 +1,9 @@
-wdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
-clearver=$(/bin/sh $wdir/openglver.sh | sed "s/\.//")
-noglxpicom=$HOME/.config/noglxpicom.conf
-[[ -a $XDG_CONFIG_HOME ]] && noglxpicom=$XDG_CONFIG_HOME/noglxpicom.conf;
+wdir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd -P)
+clearver=$(/bin/sh "$wdir/openglver.sh" | sed "s/\.//")
+noglxpicom=${XDG_CONFIG_HOME:-$HOME/.config}/noglxpicom.conf
 
-if [[ "$clearver" -ge "33" ]]
-then 
+if [[ "$clearver" -ge "33" ]]; then
   picom -b
-else 
-  picom -b --config $noglxpicom
+else
+  picom -b --config "$noglxpicom"
 fi
