df054c9caae8322850091fc8b70d7ad73729129c
Auerd <by3kov8roslav@gmail.com>
2024-05-20 21:43:52 +0400

    nvim. "options.lua" was formatted

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,9 +1,11 @@
 local cmd = vim.cmd
 local o = vim.o
 
+-- Colors
 cmd.colorscheme "github_dark_high_contrast"
 o.termguicolors = true
 
+-- Number of line
 o.number = true
 
 -- Tabs
@@ -15,9 +17,10 @@ o.shiftwidth = 2
 -- Statusline
 o.laststatus = 3
 
+-- End of buffer character
 o.fcs = "eob: "
 
--- But vim.wo.number = false works for all buffers
+-- But "vim.wo.number = false" works for all buffers
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = { "*" },
   command = "setlocal nonumber norelativenumber",
