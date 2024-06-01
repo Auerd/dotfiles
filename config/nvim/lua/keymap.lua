b403faa03e0652e4368f08681f2decb243822037
Auerd <by3kov8roslav@gmail.com>
2024-06-01 19:14:50 +0400

    feat(nvim): new buffer keymaps was added

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -1,6 +1,6 @@
 -- vim:fileencoding=utf-8:foldmethod=marker
--- Warning! This file executed before nvim. That means you can't use plugins there
--- If it's needed to use plugin in keymap, you can map key in the init() function of plugin
+-- Warning! This file executed before "lazy.nvim". That means plugins may not work there
+-- If it's needed to use plugin in "keymap()", you can map key in the "init()" function of plugin
 vim.cmd.let "mapleader = ' '"
 
 local map = vim.keymap.set
@@ -13,8 +13,25 @@ map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 -- }}}
 
 -- Buffer navigation {{{
+local bp = vim.cmd.bp
+local bn = vim.cmd.bn
 map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Go to next buffer" })
 map("n", "<S-Tab>", "<cmd>bprev<cr>", { desc = "Go to previous buffer" })
+map("n", "<leader>dd", function()
+  require("bufdelete").bufdelete(0)
+end, { desc = "Delete current buffer and move to next" })
+map("n", "<leader>dc", function()
+  require("bufdelete").bufdelete(0)
+  bp()
+end, { desc = "Delete current buffer and move to previous" })
+map("n", "<leader>dp", function()
+  bp()
+  require("bufdelete").bufdelete(0)
+end, { desc = "Delete previous buffer" })
+map("n", "<leader>dn", function()
+  bn()
+  require("bufdelete").bufdelete(0)
+end, { desc = "Delete next buffer" })
 -- }}}
 
 -- View navigation {{{
@@ -27,28 +44,32 @@ map("n", "<A-l>", "zl", { desc = "Move view to the right" })
 -- Neotree toggle {{{
 -- But you can use this workaround
 map("n", "<leader>e", function()
-  return ":Neotree position=right toggle=true<CR>"
-end, { desc = "Toggle neo-tree", expr = true })
+  require("neo-tree.command").execute { toggle = true }
+end, { desc = "Toggle neo-tree" })
 -- }}}
 
 -- Lsp {{{
-map("n", "<leader>lr", function()
+map("n", "<leader>i", function()
   return ":IncRename " .. vim.fn.expand "<cword>"
 end, { desc = "Lsp rename", expr = true })
 
-map("n", "<leader>lf", function()
+map("n", "<leader>u", function()
   vim.lsp.buf.references()
 end, { desc = "Lsp references" })
 
-map("n", "<leader>ld", function()
+map("n", "<leader>o", function()
   vim.lsp.buf.definition()
 end, { desc = "Lsp definitions" })
 -- }}}
 
 -- Numbers toggle {{{
 map("n", "<leader>nn", function()
-  return vim.o.number and "<cmd>setlocal nonumber<cr>" or "<cmd>setlocal number<cr>"
-end, { desc = "Toggle lines number", expr = true })
+  if vim.o.number then
+    vim.cmd.set "nonumber"
+  else
+    vim.cmd.set "number"
+  end
+end, { desc = "Toggle lines number" })
 -- }}}
 
 -- Mode switch {{{
--- config/nvim/lua/plugins/bufdelete.lua
+++ config/nvim/lua/plugins/bufdelete.lua
@@ -1,10 +1,6 @@
+local map = vim.keymap.set
 return {
   {
     "famiu/bufdelete.nvim",
-    init = function()
-      vim.keymap.set("n", "<leader>d", function()
-        require("bufdelete").bufdelete(0)
-      end, { desc = "Delete current buffer and move to next" })
-    end,
   },
 }
