d2de0bb2bf5a99601e5dcb269ae004bcba114628
Auerd <by3kov8roslav@gmail.com>
2024-07-12 14:47:15 +0400

    fix(nvim): shfmt doesn't work in bash and zsh files

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,10 +1,17 @@
 return {
   {
     "stevearc/conform.nvim",
+    config = function()
+      require("conform").formatters.shfmt = {
+        prepend_args = "-ln=auto",
+      }
+    end,
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
+        sh = { "shfmt" },
         bash = { "shfmt" },
+        zsh = { "shfmt" },
       },
       format_on_save = {
         timeout_ms = 500,
