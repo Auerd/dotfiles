b02566464977d7e00d3246dccea0f7d84cf3b89c
Auerd <by3kov8roslav@gmail.com>
2024-08-18 17:20:28 +0400

    feat(nvim): telescope.nvim plugin was added

---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "master",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      defaults = {
+        border = false,
+      },
+    },
+  },
+}
