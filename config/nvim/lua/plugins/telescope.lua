29002b6d1f90bdda03931337068a9e4f3191c39e
Auerd <by3kov8roslav@gmail.com>
2024-08-18 20:56:23 +0400

    format(nvim): borders changed for telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -2,10 +2,13 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     branch = "master",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
     opts = {
       defaults = {
-        border = false,
+        borderchars = { " ", "", "", "", "", "", "", "" },
       },
     },
   },
