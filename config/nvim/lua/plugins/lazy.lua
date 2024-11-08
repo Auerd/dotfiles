e897c5f71d545ef9ab23eb1a4c431a8f2436bcf8
Auerd <by3kov8roslav@gmail.com>
2024-11-08 20:30:25 +0400

    feat(nvim): checker enabled

--- config/nvim/lua/plugins/lazy.lua
+++ config/nvim/lua/plugins/lazy.lua
@@ -5,12 +5,12 @@ return {
     opts = {
       checker = {
         -- automatically check for plugin updates
-        enabled = false,
+        enabled = true,
         concurrency = nil, ---@type number? set to 1 to check for updates very slowly
         notify = true, -- get a notification when new updates are found
         frequency = 3600, -- check for updates every hour
         check_pinned = false, -- check for pinned packages that can't be updated
       },
-    }
+    },
   },
 }
