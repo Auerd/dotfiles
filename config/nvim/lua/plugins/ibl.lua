090f4e4655d9096a1e4d6fea66ff71bdd7e0cc75
Auerd <by3kov8roslav@gmail.com>
2025-06-23 14:54:45 +0400

    neovim: rainbow indent was added

--- config/nvim/lua/plugins/ibl.lua
+++ config/nvim/lua/plugins/ibl.lua
@@ -1,26 +1,49 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  opts = {
-    scope = { enabled = false },
-    indent = {
-      char = "┃",
-    },
-    exclude = {
-      filetypes = {
-        "lspinfo",
-        "packer",
-        "checkhealth",
-        "help",
-        "man",
-        "gitcommit",
-        "TelescopePrompt",
-        "TelescopeResults",
-        "",
-        "dashboard",
+  opts = function()
+    local hooks = require "ibl.hooks"
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+    end)
+    return {
+      scope = {
+        enabled = false,
       },
-    },
-  },
+      indent = {
+        char = "┃",
+        highlight = {
+          "RainbowRed",
+          "RainbowYellow",
+          "RainbowBlue",
+          "RainbowOrange",
+          "RainbowGreen",
+          "RainbowViolet",
+          "RainbowCyan",
+        },
+      },
+      exclude = {
+        filetypes = {
+          "lspinfo",
+          "packer",
+          "checkhealth",
+          "help",
+          "man",
+          "gitcommit",
+          "TelescopePrompt",
+          "TelescopeResults",
+          "",
+          "dashboard",
+        },
+      },
+    }
+  end,
   dependencies = {
     "raddari/last-color.nvim",
   },
