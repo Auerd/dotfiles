f74beeb0e9deff886ba2c042b87edd9c45d5dfc5
Auerd <by3kov8roslav@gmail.com>
2025-06-23 21:53:25 +0400

    neovim: formatting was changed a bit

--- config/nvim/lua/autocmds.lua
+++ config/nvim/lua/autocmds.lua
@@ -16,7 +16,9 @@ autocmd("BufWritePost", {
       return
     end
     local filename = data.file:match "(.+)%..+"
-    vim.fn.jobstart("sass " .. filename .. ".scss " .. filename .. ".css")
+    local infile = filename .. ".scss"
+    local outfile = filename .. ".css"
+    vim.fn.jobstart("sass " .. infile .. " " .. outfile)
   end,
 })
 -- }}}
@@ -25,7 +27,11 @@ autocmd("BufWritePost", {
 autocmd("BufWritePre", {
   pattern = "*",
   callback = function(args)
-    require("conform").format({ bufnr = args.buf, timeout_ms = 5000, lsp_format = "fallback" }, function(err, did_edit)
+    require("conform").format({
+      bufnr = args.buf,
+      timeout_ms = 5000,
+      lsp_format = "fallback",
+    }, function(err, did_edit)
       if err == nil and did_edit then
         doautocmds("User", { pattern = "BufLintAfter" })
       end
@@ -40,17 +46,18 @@ local function setind2(buf)
   buf.tabstop = 2
   buf.shiftwidth = 2
 end
+local function setindtabs(buf)
+  buf.expandtab = false
+  buf.tabstop = 4
+  buf.shiftwidth = 4
+end
 local indents = {
   lua = setind2,
   c = setind2,
   cpp = setind2,
   html = setind2,
   css = setind2,
-  sh = function(buf)
-    buf.expandtab = false
-    buf.tabstop = 4
-    buf.shiftwidth = 4
-  end,
+  sh = setindtabs,
 }
 autocmd("BufEnter", {
   callback = function(data)
