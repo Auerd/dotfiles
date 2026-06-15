d2a5185a9ff470276a3ce355bc32a11adc65a89f
Yaroslav Bychkov <by3kov8roslav@gmail.com>
2026-06-15 13:49:09 +0000

    fix(treesitter): some config errors

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,113 +1,72 @@
 -- source: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/treesitter.lua
 -- I had big headache with it
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    version = false,
-    build = ":TSUpdate",
-    event = { "VeryLazy", "BufReadPost", "BufWritePost", "BufNewFile" },
-    init = function(plugin)
-      -- source: https://github.com/nvim-treesitter/nvim-treesitter/issues/655
-      vim.treesitter.language.register("bash", "zsh")
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require "nvim-treesitter.query_predicates"
-    end,
-    dependencies = {
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        config = function()
-          local move = require "nvim-treesitter.textobjects.move" ---@type table<string,fun(...)>
-          local configs = require "nvim-treesitter.configs"
-          for name, fn in pairs(move) do
-            if name:find "goto" == 1 then
-              move[name] = function(q, ...)
-                if vim.wo.diff then
-                  local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
-                  for key, query in pairs(config or {}) do
-                    if q == query and key:find "[%]%[][cC]" then
-                      vim.cmd("normal! " .. key)
-                      return
-                    end
-                  end
-                end
-                return fn(q, ...)
-              end
-            end
-          end
-        end,
-      },
-      "raddari/last-color.nvim",
+  "nvim-treesitter/nvim-treesitter",
+  version = false,
+  build = ":TSUpdate",
+  event = { "VeryLazy", "BufReadPost", "BufWritePost", "BufNewFile" },
+  init = function(plugin)
+    -- source: https://github.com/nvim-treesitter/nvim-treesitter/issues/655
+    vim.treesitter.language.register("bash", "zsh")
+  end,
+  dependencies = {
+    "raddari/last-color.nvim",
+  },
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  keys = {
+    { "<c-space>", desc = "Increment Selection" },
+    { "<bs>", desc = "Decrement Selection", mode = "x" },
+  },
+  ---@type TSConfig
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    sync_install = false,
+    highlight = {
+      enable = true,
+      disable = { "latex" },
+      additional_vim_regex_highlighting = false,
     },
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    keys = {
-      { "<c-space>", desc = "Increment Selection" },
-      { "<bs>", desc = "Decrement Selection", mode = "x" },
+    indent = { enable = false },
+    ensure_installed = {
+      "c",
+      "cpp",
+      "rust",
+      "make",
+      "cmake",
+      "xcompose",
+      "python",
+      "bash",
+      "xml",
+      "toml",
+      "yaml",
+      "arduino",
+      "java",
+      "html",
+      "css",
+      "javascript",
+      "gitcommit",
+      "git_config",
+      "git_rebase",
+      "gitattributes",
+      "gitignore",
     },
-    ---@type TSConfig
-    ---@diagnostic disable-next-line: missing-fields
-    opts = {
-      sync_install = false,
-      highlight = {
-        enable = true,
-        disable = { "latex" },
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = false },
-      ensure_installed = {
-        "c",
-        "cpp",
-        "rust",
-        "make",
-        "cmake",
-        "xcompose",
-        "python",
-        "bash",
-        "xml",
-        "toml",
-        "yaml",
-        "arduino",
-        "java",
-        "html",
-        "css",
-        "javascript",
-        "gitcommit",
-        "git_config",
-        "git_rebase",
-        "gitattributes",
-        "gitignore",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-      textobjects = {
-        move = {
-          enable = false,
-          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-        },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
       },
     },
-    config = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        ---@type table<string, boolean>
-        local added = {}
-        opts.ensure_installed = vim.tbl_filter(function(lang)
-          if added[lang] then
-            return false
-          end
-          added[lang] = true
-          return true
-        end, opts.ensure_installed)
-      end
-      require("nvim-treesitter.configs").setup(opts)
-    end,
+    textobjects = {
+      move = {
+        enable = false,
+        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+      },
+    },
   },
 }
